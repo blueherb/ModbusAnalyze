c7b75685c2c57a04fed77260b5b45840acc8bbdc
blueherb <red_herb@naver.com>
2022-07-13 17:57:01 +0900

    오류 수정 및 정리
    오류없이 형식변환 시키는 것에 중점을 두어 작업 실시

--- AnalyzeModbus/MainForm.cs
+++ AnalyzeModbus/MainForm.cs
@@ -22,18 +22,45 @@ namespace AnalyzeModbus
         public static string rx;
         public static string tx;
         public static string err;
-        public static int convertedValue;
-        TimeSpan tcpConnectedTime;
-
+        public static int intValue;
+        private string timeNow = DateTime.Now.ToString("yyMMddHHmmss");
+        private string timeDifference;
+     
         public MainForm()
         {
             InitializeComponent();
         }
 
+        //ModbusTCP 연결 탭 열기
+        private void tsmiModbusTcp_Click(object sender, EventArgs e)
+        {
+            ModbusTcpForm modbusTcpForm = new ModbusTcpForm();
+            if (modbusTcpForm.ShowDialog() == DialogResult.OK)
+                log.Info("*ModbusTCP 연결 탭 열림");
+        }
+
+        //String -> Int32 Hex 오류없이 변환. (작업중!)
+        public static string Str2hex(string str2hex)
+        {
+            string resultHex = string.Empty;
+            byte[] arr_byteStr = Encoding.Default.GetBytes(str2hex);
+
+            foreach (byte byteStr in arr_byteStr)
+                resultHex += string.Format("{0:X2}", byteStr);
+
+            return resultHex;
+        }
+        
+        //String -> int32 Dex 오류없이 변환. (작업중!)
+        public static string Str2Dex(string str2dex)
+        {
+            string resultDex = string
+        }
         //MainForm 로드 이벤트 발생 시 이전 작업 복구
         private void MainForm_Load(object sender, EventArgs e)
         {
             log.Info("[프로그램 시작]");
+
             tbQuery.Text = Properties.Settings.Default.queryNumber;
             cbFunctionCoil.Text = Properties.Settings.Default.functionCoil;
             tbMemoryStart.Text = Properties.Settings.Default.startMemory;
@@ -44,49 +71,27 @@ namespace AnalyzeModbus
             cbQueryRule.Text = Properties.Settings.Default.queryRule;
             tbRapidCount.Text = Properties.Settings.Default.rapidCount;
             dudTx.Text = Properties.Settings.Default.sendedData;
-            StrCheck(tbQuery.Text);
-            StrCheck(cbFunctionCoil.Text);
-            StrCheck(tbMemoryStart.Text);
-            StrCheck(tbMemoryLength.Text);
-            StrCheck(tbTxInterval.Text);
-            StrCheck(cbQueryRule.Text);
-            StrCheck(tbRapidCount.Text);
-            StrCheck(dudTx.Text);
-
-            LeftTime();
 
             log.Info("*이전 임시설정 복구");
         }
-        public static string StrCheck (string value)
-        {
-            if (value == "")
-            {
-                value = "1";
-            }
-            else if (!Int32.TryParse(value, out convertedValue))
-            {
-                value = "1";
-            }
-            return value;
-        }
-        //자동 송신 예상시간 계산
+       
+        //자동 송신 예상시간 계산 (오류! 수정중!)
         private void leftTime_Changed(object sender, EventArgs e)
         {
-            LeftTime();
-
+            string leftTime = Convert.ToString (StrCheck(tbTxInterval.Text)*StrCheck(tbRapidCount.Text)/ 1000);
+            lbLeftTime.Text = DateTime.Parse(leftTime).ToString("ddHHmmss");
         }
-        public void LeftTime()
+        //시간 차 구하기
+        public string TimeDifference(string now, string before)
         {
-            TimeSpan leftTime = TimeSpan.FromSeconds(Convert.ToDouble(tbTxInterval.Text) / Convert.ToDouble(1000) * Convert.ToDouble(tbRapidCount.Text));
-            lbLeftTime.Text = leftTime.ToString(@"hh\-mm\-ss\");
+            timeDifference = (Convert.ToDateTime(now) - Convert.ToDateTime(before)).ToString("yy MM dd HH mm ss");
+            return timeDifference;
         }
 
-        //ModbusTCP 연결 탭 열기
-        private void tsmiModbusTcp_Click(object sender, EventArgs e)
+        //ModbusTCP 통신 진행시간 계산
+        private void timer1_Tick(object sender, EventArgs e)
         {
-            ModbusTcpForm modbusTcpForm = new ModbusTcpForm();
-            if (modbusTcpForm.ShowDialog() == DialogResult.OK)
-                log.Info("*ModbusTCP 연결 탭 열림");
+            TimeDifference(lbStartTime.Text, timeNow);
         }
 
         //연결 시 스탯 표시
@@ -98,20 +103,13 @@ namespace AnalyzeModbus
                 lbIp.Text = Config.sTcpIpAddress;
                 lbPort.Text = Config.sTcpPort;
                 lbMode.Text = "ModbusTCP";
-                lbStartTime.Text = DateTime.Now.ToString("yyyy-MM-dd hh-mm-ss");
+                lbStartTime.Text = timeNow;
+                lbConnectTime.Text = timeDifference;
                 lbRxCount.Text = rx;
                 lbTxCount.Text = tx;
                 lbErrCount.Text = err;
                 log.Info("*연결 상태 모니터 활성화");
             }
-        }
-
-        //ModbusTCP 통신 시간차 계산
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            tcpConnectedTime = DateTime.Now - Convert.ToDateTime(lbStartTime.Text);
-            Convert.ToDateTime(tcpConnectedTime);
-            lbConnectTime.Text = tcpConnectedTime.ToString("dd hh-mm-ss");
         }
 
         //연결 취소
@@ -124,8 +122,8 @@ namespace AnalyzeModbus
                 log.Info("*연결 끊음"+"[Mode : ModbusTCP, IP :"+Config.sTcpIpAddress+", Port : "+Config.sTcpIpAddress
                 +"Connected time : "+lbConnectTime.Text+"]");
             }
-
         }
+
         //설정 저장
         private void tsmiSave_Click(object sender, EventArgs e)
         {
